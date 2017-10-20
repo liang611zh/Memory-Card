60573a4c410fe4f85f12145926a99f728386ec57
LIANG ZHAO <liangzhao@node-1w7jr9qqdi59vt8oixucol81a.ipv6.telus.net>
2017-10-19 21:04:41 -0700

    Initial Commit

---
+++ MemoryCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryCard
+//
+//  Created by LIANG ZHAO on 2017-10-19.
+//  Copyright © 2017 bcit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
